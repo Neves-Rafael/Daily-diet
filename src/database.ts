57c297d42dad41b4f8ea094e92066dab959ba7ee
Rafael Neves <nevesrafael.dev@gmail.com>
2024-04-29 21:57:22 -0300

    :sparkles: Feat knex config

---
+++ src/database.ts
@@ -0,0 +1,15 @@
+import { knex as knexSetup, Knex } from "knex"
+
+export const config: Knex.Config = {
+  client: "sqlite",
+  connection: {
+    filename: "./db/app.db"
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: "ts",
+    directory: "./db/migrations"
+  }
+}
+
+export const knex = knexSetup(config)
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import fastify from "fastify";
+import { transaction } from "./app";
+import { knex }from "./database";
 
-const server = fastify()
+const app = fastify()
 
-server.listen({
+app.get("/hello", async() => {
+  const test = await knex("sqlite_schema").select("*")
+  return test
+})
+
+app.listen({
   port: 3333
 }).then(() => {
   console.log("HTTP Server Running 🔥🔥🔥")
