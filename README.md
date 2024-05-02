2a42ac3b088e0b8b0843c992797ec77fe498936c
Rafael Neves <nevesrafael.dev@gmail.com>
2024-05-01 21:06:15 -0300

    :sparkles: Feat some meal routes

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 - [x] Deve ser possível criar um usuário
 - [ ] Deve ser possível identificar o usuário entre as requisições
-- [ ] Deve ser possível registrar uma refeição feita, com as seguintes informações:
+- [x] Deve ser possível registrar uma refeição feita, com as seguintes informações:
       *As refeições devem ser relacionadas a um usuário.*
       - Nome
       - Descrição
@@ -10,9 +10,9 @@
       - Está dentro ou não da dieta
 
 - [ ] Deve ser possível editar uma refeição, podendo alterar todos os dados acima
-- [ ] Deve ser possível apagar uma refeição
+- [x] Deve ser possível apagar uma refeição
 - [ ] Deve ser possível listar todas as refeições de um usuário
-- [ ] Deve ser possível visualizar uma única refeição
+- [x] Deve ser possível visualizar uma única refeição
 - [ ] Deve ser possível recuperar as métricas de um usuário
     - [ ] Quantidade total de refeições registradas
     - [ ] Quantidade total de refeições dentro da dieta
--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -4,7 +4,69 @@ import { z } from "zod"
 import { knex } from "../database";
 
 export async function mealRoutes(app: FastifyInstance){
-  app.post("/", async () => {
+  app.post("/", async (request, reply) => {
+    const mealSchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      diet: z.boolean()
+    })
+
+    const { name, description, diet} = mealSchema.parse(request.body)
+
+    await knex("meal").insert({
+      user_id: randomUUID(),
+      name,
+      description,
+      diet,
+      updated_at: new Date(),
+      created_at: new Date()
+    })
+
+    return reply.status(201).send()
+
+  })
+
+  app.get("/:id", async (request, reply) => {
+    const idSchema = z.object({
+      id: z.string(),
+    })
+
+    const { id } = idSchema.parse(request.params)
+    
+    const mealSearch = await knex("meal").where({ id }).first()
+
+    if(!mealSearch){
+      return reply.status(404).send("Meal not found!")
+    }
+
+    return reply.status(200).send(mealSearch)
+  })
+
+  app.get("/,", async () => {
+
+  })
+
+  app.delete("/:id", async (request, reply) => {
+    const idSchema = z.object({
+      id: z.string(),
+    })
+
+    const { id } = idSchema.parse(request.params)
+    
+    const mealSearch = await knex("meal").where({ id }).delete()
+
+    if(!mealSearch){
+      return reply.status(404).send("Meal not found!")
+    }
+
+    return reply.status(200).send()
+  })
+
+  app.put("/:id", async () => {
+
+  })
+
+  app.get("/summary",  async () => {
 
   })
 }
