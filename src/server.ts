70f630dc8d89bc4e581e01ecfe48f1e4dc677ab8
Rafael Neves <nevesrafael.dev@gmail.com>
2024-05-04 23:29:54 -0300

    :bug: Fix host

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,8 @@ import { env } from "./env"
 
 
 app.listen({
-  port: env.PORT
+  port: env.PORT,
+  host: 'RENDER' in process.env ? `0.0.0.0` : `localhost`,
 }).then(() => {
   console.log(`HTTP Server Running Port ${env.PORT} 🔥🔥🔥`)
 })
