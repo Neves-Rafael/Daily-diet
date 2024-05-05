5079bc36b246c67346dc718b97dd5838dd73cf6d
Rafael Neves <nevesrafael.dev@gmail.com>
2024-05-04 23:56:00 -0300

    :bug: Fix node version

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,10 @@
 import { knex as knexSetup, Knex } from "knex"
 import { env } from "./env"
 
+if(!process.env.DATABASE_URL){
+  throw new Error("DATABASE_URL not found in env file")
+}
+
 export const config: Knex.Config = {
   client: env.DATABASE_CLIENT,
   connection: env.DATABASE_CLIENT === "sqlite" ? { filename: env.DATABASE_URL} : env.DATABASE_URL,
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import { app } from "./app"
 import { env } from "./env"
 
-
 app.listen({
   port: env.PORT,
   host: 'RENDER' in process.env ? `0.0.0.0` : `localhost`,
