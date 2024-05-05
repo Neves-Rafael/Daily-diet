2d45031e35bc1d1c9271c85d6da84a08d9c6016d
Rafael Neves <nevesrafael.dev@gmail.com>
2024-05-05 20:54:02 -0300

    :bug: Test migrations

--- db/migrations/20240430005025_create-user.ts
+++ db/migrations/20240430005025_create-user.ts
@@ -3,11 +3,7 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable("users", (table) => {
     table.uuid("id").primary()
-    table.text("name").notNullable()
-    table.text("email").notNullable()
-    table.text("password").notNullable()
-    table.timestamp("updated_at").defaultTo(knex.fn.now())
-    table.timestamp("created_at").defaultTo(knex.fn.now())
+
   })
 }
 
@@ -16,3 +12,9 @@ export async function down(knex: Knex): Promise<void> {
   await knex.schema.dropTable("users")
 }
 
+
+// table.string("name").notNullable()
+// table.string("email").notNullable()
+// table.string("password").notNullable()
+// table.timestamp("updated_at").defaultTo(knex.fn.now())
+// table.timestamp("created_at").defaultTo(knex.fn.now())
--- db/migrations/20240430142409_create-meal.ts
+++ db/migrations/20240430142409_create-meal.ts
@@ -1,20 +1,19 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable("meal", (table) => {
     table.increments("id").primary()
-    table.uuid("user_id").references("id").inTable("users").onDelete("CASCADE").notNullable()
-    table.text("name").notNullable()
-    table.text("description")
-    table.boolean("diet").notNullable()
-    table.timestamp("updated_at").defaultTo(knex.fn.now())
-    table.timestamp("created_at").defaultTo(knex.fn.now())
   })
 }
-
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.dropTable("meal")
 }
 
+
+// table.uuid("user_id").references("id").inTable("users").onDelete("CASCADE").notNullable()
+// table.text("name").notNullable()
+// table.text("description")
+// table.boolean("diet").notNullable()
+// table.timestamp("updated_at").defaultTo(knex.fn.now())
+// table.timestamp("created_at").defaultTo(knex.fn.now())
