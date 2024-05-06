defee801ff761a0a5c76ef3aa1ea88cf57efd5a1
Rafael Neves <nevesrafael.dev@gmail.com>
2024-05-05 21:02:32 -0300

    :sparkles: Update migrations

--- db/migrations/20240430005025_create-user.ts
+++ db/migrations/20240430005025_create-user.ts
@@ -3,7 +3,11 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable("users", (table) => {
     table.uuid("id").primary()
-
+    table.string("name").notNullable()
+    table.string("email").notNullable()
+    table.string("password").notNullable()
+    table.timestamp("updated_at").defaultTo(knex.fn.now())
+    table.timestamp("created_at").defaultTo(knex.fn.now())
   })
 }
 
@@ -12,9 +16,3 @@ export async function down(knex: Knex): Promise<void> {
   await knex.schema.dropTable("users")
 }
 
-
-// table.string("name").notNullable()
-// table.string("email").notNullable()
-// table.string("password").notNullable()
-// table.timestamp("updated_at").defaultTo(knex.fn.now())
-// table.timestamp("created_at").defaultTo(knex.fn.now())
